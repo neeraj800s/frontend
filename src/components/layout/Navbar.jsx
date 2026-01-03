a62b09ac6b95919865ed81cdfc973bd9772873e4
neeraj <sneerajsingh91@gmail.com>
2026-01-03 23:39:36 +0530

    hjik

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
           : 'bg-white/80 backdrop-blur-md'
           }`}
       >
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             <Link to="/" className="flex items-center space-x-2">
